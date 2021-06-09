e403a8d711e3c44e42f37bb32c71163a65087f12
Liamb17 <60904241+Liamb17@users.noreply.github.com>
2021-06-09 16:25:26 +0100

    refactor: Split 2 versions of division script into separate files

--- numberdivision.py
+++ numberdivision.py
@@ -1,26 +1,4 @@
-def divide_version1(num1,num2):
-    # Verify input 1 is in numeric form
-    if type(num1) != int and float:
-        # Input is not numeric, return error to user
-        return 'Error - Input1 is not a number'
-    
-    # Verify input 2 is in numeric form
-    elif type(num2) != int and float:
-        # Input is not numeric, return error to user
-        return 'Error - Input2 is not a number'
-        
-    # Check if the denominator number is greater than 0
-    if (num2 == 0):
-        # Denominator is 0, return error to user
-        return 'Error - You cannot divide by 0. Please choose an appropriate denominator'
-    
-    # Perform the devision
-    divide_res = num1/num2;
-    
-    # Return completed equasion to user 
-    return "{0}/{1} = {2}".format(num1,num2,divide_res)
-    
-def divide_version2(num1,num2):
+def divide(num1,num2):
     # Verify inputs are in numeric form
     if (type(num1) != int and float) or (type(num2) != int and float):
         # Input are not numeric, return error to user
